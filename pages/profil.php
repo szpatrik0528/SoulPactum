6421493cf2d33e89eb828023a5264eb1900d9b35
szpatrik0528 <szpatrik.photoshop@gmail.com>
2024-03-27 12:12:22 +0100

    main

--- pages/profil.php
+++ pages/profil.php
@@ -5,13 +5,13 @@
         if ($row !== null) {
             $card = '<div class="card mx-auto" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title align-items-center">' . $row['username'] . '</h5>' .
-                    '<p class="card-text">Email cím: ' . $row['emailcim'] . '</p>' .
-                    '<p class="card-text">Teljes név: ' . $row['teljesnev'] . '</p>' .
-                    '<p class="card-text">Telefonszám: ' . $row['telefonszam'] . '</p>' .
-                    '<p class="card-text">Irányítószám: ' . $row['iranyitoszam'] . '</p>' .
-                    '<p class="card-text">Település: ' . $row['telepules'] . '</p>' .
-                    '<p class="card-text">Cím: ' . $row['cim'] . '</p>
+                        <h5 class="card-title text-center">' . $row['username'] . '</h5>' .
+                '<p class="card-text">Email cím: ' . $row['emailcim'] . '</p>' .
+                '<p class="card-text">Teljes név: ' . $row['teljesnev'] . '</p>' .
+                '<p class="card-text">Telefonszám: ' . $row['telefonszam'] . '</p>' .
+                '<p class="card-text">Irányítószám: ' . $row['iranyitoszam'] . '</p>' .
+                '<p class="card-text">Település: ' . $row['telepules'] . '</p>' .
+                '<p class="card-text">Cím: ' . $row['cim'] . '</p>
                     </div>
                 </div>';
             echo $card;
